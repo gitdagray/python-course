b654fed2b6bf967848c2eb896a3b84ceaf1b9d37
gitdagray <DaveOnEleven@gmail.com>
2023-02-15 07:27:23 -0600

    Added lesson 2

---
+++ lesson02/welcome.py
@@ -0,0 +1,12 @@
+line01 = "********************"  # header / footer
+line02 = "*                  *"  # re-use
+line03 = "*     WELCOME!     *"
+
+
+# starts with a blank line
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
