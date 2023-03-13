1256a06e724182511e0fe83cf33888d5d33080ce
gitdagray <DaveOnEleven@gmail.com>
2023-03-13 18:10:20 -0500

    Added lesson 5

---
+++ lesson05/rps.py
@@ -0,0 +1,39 @@
+import sys
+import random
+from enum import Enum
+
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+print("")
+playerchoice = input(
+    "Enter...\n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
+
+player = int(playerchoice)
+
+if player < 1 | player > 3:
+    sys.exit("You must enter 1, 2, or 3.")
+
+computerchoice = random.choice("123")
+
+computer = int(computerchoice)
+
+print("")
+print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
+print("Python chose " + str(RPS(computer)).replace('RPS.', '') + ".")
+print("")
+
+if player == 1 and computer == 3:
+    print("🎉 You win!")
+elif player == 2 and computer == 1:
+    print("🎉 You win!")
+elif player == 3 and computer == 2:
+    print("🎉 You win!")
+elif player == computer:
+    print("😲 Tie game!")
+else:
+    print("🐍 Python wins!")
