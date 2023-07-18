07dab2d957bfce7ea12c346e51c135ae1fa2bbfc
gitdagray <DaveOnEleven@gmail.com>
2023-07-18 07:50:45 -0500

    Added lesson 23

---
+++ lesson23/server.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from weather import get_current_weather
+from waitress import serve
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/weather')
+def get_weather():
+    city = request.args.get('city')
+
+    # Check for empty strings or string with only spaces
+    if not bool(city.strip()):
+        city = "Kansas City"
+
+    weather_data = get_current_weather(city)
+
+    # City is not found by API
+    if not weather_data['cod'] == 200:
+        return render_template('city-not-found.html')
+
+    return render_template(
+        "weather.html",
+        title=weather_data["name"],
+        status=weather_data["weather"][0]["description"].capitalize(),
+        temp=f"{weather_data['main']['temp']:.1f}",
+        feels_like=f"{weather_data['main']['feels_like']:.1f}"
+    )
+
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8000)
---
+++ lesson23/weather.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+
+def get_current_weather(city="Kansas City"):
+
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+
+
+if __name__ == "__main__":
+    print('\n*** Get Current Weather Conditions ***\n')
+
+    city = input("\nPlease enter a city name: ")
+
+    # Check for empty strings or string with only spaces
+    if not bool(city.strip()):
+        city = "Kansas City"
+
+    weather_data = get_current_weather(city)
+
+    print("\n")
+    pprint(weather_data)
